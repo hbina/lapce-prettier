e71d21aa6860536acc9bbc85a0aef7d9494ed5b7
Hanif Bin Ariffin <hanif.ariffin.4326@gmail.com>
2023-01-31 23:07:39 +0800

    First commit
    This is a first version  that does not try to resolve NPM packages.
    Instead, it relies on `prettier` being a binary which is possible
    with something like nexe or pkg.
    
    Signed-off-by: Hanif Bin Ariffin <hanif.ariffin.4326@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,130 +1,124 @@
-// Deny usage of print and eprint as it won't have same result
-// in WASI as if doing in standard program, you must really know
-// what you are doing to disable that lint (and you don't know)
-#![deny(clippy::print_stdout)]
-#![deny(clippy::print_stderr)]
-
-use anyhow::Result;
 use lapce_plugin::{
     psp_types::{
-        lsp_types::{request::Initialize, DocumentFilter, DocumentSelector, InitializeParams, Url, MessageType},
-        Request,
+        lsp_types::{
+            request::{Formatting, Initialize},
+            DocumentFormattingParams, InitializeParams, InitializeResult, MessageType, OneOf,
+            ServerCapabilities,
+        },
+        ExecuteProcessResult, Request,
     },
-    register_plugin, LapcePlugin, VoltEnvironment, PLUGIN_RPC,
+    register_plugin, LapcePlugin, PLUGIN_RPC,
 };
 use serde_json::Value;
 
-#[derive(Default)]
-struct State {}
+#[derive(Default, Debug)]
+struct State {
+    prettier_path: Option<String>,
+}
 
 register_plugin!(State);
 
-fn initialize(params: InitializeParams) -> Result<()> {
-    let document_selector: DocumentSelector = vec![DocumentFilter {
-        // lsp language id
-        language: Some(String::from("language_id")),
-        // glob pattern
-        pattern: Some(String::from("**/*.{ext1,ext2}")),
-        // like file:
-        scheme: None,
-    }];
-    let mut server_args = vec![];
-
-    // Check for user specified LSP server path
-    // ```
-    // [lapce-plugin-name.lsp]
-    // serverPath = "[path or filename]"
-    // serverArgs = ["--arg1", "--arg2"]
-    // ```
-    if let Some(options) = params.initialization_options.as_ref() {
-        if let Some(lsp) = options.get("lsp") {
-            if let Some(args) = lsp.get("serverArgs") {
-                if let Some(args) = args.as_array() {
-                    if !args.is_empty() {
-                        server_args = vec![];
-                    }
-                    for arg in args {
-                        if let Some(arg) = arg.as_str() {
-                            server_args.push(arg.to_string());
-                        }
+impl State {
+    fn handle_init(&mut self, params: &InitializeParams) -> Result<Value, String> {
+        if let Some(opts) = params.initialization_options.as_ref() {
+            if let Some(server_path) = opts.get("prettierPath") {
+                if let Some(server_path) = server_path.as_str() {
+                    if !server_path.is_empty() {
+                        self.prettier_path = Some(server_path.to_string())
                     }
                 }
             }
+        }
 
-            if let Some(server_path) = lsp.get("serverPath") {
-                if let Some(server_path) = server_path.as_str() {
-                    if !server_path.is_empty() {
-                        let server_uri = Url::parse(&format!("urn:{}", server_path))?;
-                        PLUGIN_RPC.start_lsp(
-                            server_uri,
-                            server_args,
-                            document_selector,
-                            params.initialization_options,
-                        );
-                        return Ok(());
+        let server_params = InitializeResult {
+            capabilities: ServerCapabilities {
+                document_formatting_provider: Some(OneOf::Left(true)),
+                ..ServerCapabilities::default()
+            },
+            ..InitializeResult::default()
+        };
+
+        serde_json::to_value(server_params).map_err(|s| s.to_string())
+    }
+
+    fn handle_formatting(&self, id: u64, params: &DocumentFormattingParams) -> Result<(), String> {
+        let file_path = params
+            .text_document
+            .uri
+            .to_file_path()
+            .map_err(|_| format!("'{}' is not a valid file path", params.text_document.uri))?
+            .to_str()
+            .ok_or(format!(
+                "'{}' is not a valid unicode string",
+                params.text_document.uri
+            ))?
+            .to_string();
+
+        let prettier_path = self.prettier_path.clone().unwrap_or("prettier".to_string());
+
+        PLUGIN_RPC.window_log_message(
+            MessageType::LOG,
+            format!("file_path:{file_path} prettier_path:{prettier_path}"),
+        );
+        // Hack because we can't send the document in the result
+        std::thread::sleep(std::time::Duration::from_secs(2));
+        match PLUGIN_RPC
+            .execute_process(
+                prettier_path,
+                vec![
+                    "--write".to_string(), // Remove this once `execute_process` supports stdout/stderr
+                    file_path,
+                ],
+            )
+            .map(|ExecuteProcessResult { success }| Value::Bool(success))
+            .map_err(|e| e.to_string())
+        {
+            Ok(params) => PLUGIN_RPC.host_success(id, params),
+            Err(err) => PLUGIN_RPC.host_error(id, err),
+        }
+
+        Ok(())
+    }
+}
+
+impl LapcePlugin for State {
+    fn handle_request(&mut self, id: u64, method: String, client_params: Value) {
+        #[allow(clippy::single_match)]
+        match method.as_str() {
+            Initialize::METHOD => {
+                let params: InitializeParams = serde_json::from_value(client_params).unwrap();
+                match self.handle_init(&params) {
+                    Ok(result) => {
+                        PLUGIN_RPC.host_success(id, result);
+                    }
+                    Err(err) => {
+                        PLUGIN_RPC.host_error(id, err);
+                    }
+                };
+            }
+            Formatting::METHOD => {
+                let params =
+                    serde_json::from_value::<DocumentFormattingParams>(client_params).unwrap();
+                match self.handle_formatting(id, &params) {
+                    Ok(_) => {}
+                    Err(err) => {
+                        PLUGIN_RPC.host_error(id, err);
                     }
                 }
+            }
+            _ => {
+                PLUGIN_RPC.host_error(
+                    id,
+                    format!("Prettier plugin does not support method '{method}'"),
+                );
             }
         }
     }
 
-    // Architecture check
-    let _ = match VoltEnvironment::architecture().as_deref() {
-        Ok("x86_64") => "x86_64",
-        Ok("aarch64") => "aarch64",
-        _ => return Ok(()),
-    };
-
-    // OS check
-    let _ = match VoltEnvironment::operating_system().as_deref() {
-        Ok("macos") => "macos",
-        Ok("linux") => "linux",
-        Ok("windows") => "windows",
-        _ => return Ok(()),
-    };
-
-    // Download URL
-    // let _ = format!("https://github.com/<name>/<project>/releases/download/<version>/{filename}");
-
-    // see lapce_plugin::Http for available API to download files
-
-    let _ = match VoltEnvironment::operating_system().as_deref() {
-        Ok("windows") => {
-            format!("{}.exe", "[filename]")
-        }
-        _ => "[filename]".to_string(),
-    };
-
-    // Plugin working directory
-    let volt_uri = VoltEnvironment::uri()?;
-    let server_uri = Url::parse(&volt_uri)?.join("[filename]")?;
-
-    // if you want to use server from PATH
-    // let server_uri = Url::parse(&format!("urn:{filename}"))?;
-
-    // Available language IDs
-    // https://github.com/lapce/lapce/blob/HEAD/lapce-proxy/src/buffer.rs#L173
-    PLUGIN_RPC.start_lsp(
-        server_uri,
-        server_args,
-        document_selector,
-        params.initialization_options,
-    );
-
-    Ok(())
-}
-
-impl LapcePlugin for State {
-    fn handle_request(&mut self, _id: u64, method: String, params: Value) {
-        #[allow(clippy::single_match)]
-        match method.as_str() {
-            Initialize::METHOD => {
-                let params: InitializeParams = serde_json::from_value(params).unwrap();
-                if let Err(e) = initialize(params) {
-                    PLUGIN_RPC.window_show_message(MessageType::ERROR, format!("plugin returned with error: {e}"))
-                }
-            }
-            _ => {}
-        }
+    fn handle_notification(&mut self, method: String, params: Value) {
+        PLUGIN_RPC.window_log_message(
+            MessageType::LOG,
+            format!("handle_notification method:{method} params:{params}"),
+        );
     }
 }
